0e21f4599d91dc76dfbce08d963291317c5596cd
vikrantsingh13 <vikrantsinghvm@gmail.com>
2017-04-19 18:53:21 -0400

    playing around with the tools

--- js/index.js
+++ js/index.js
@@ -1,18 +1,7 @@
-function adjectifier(adjective){
-	return function(string){
-		return adjective + " " + string;
-	};
+var typesOfBalls = ['basket', 'base', 'foot'];
+
+for(var i = 0; i < typesOfBalls.length; i++){
+	var listElement = document.createElement('li');
+	listElement.innerHTML = typesOfBalls[i];
+	document.getElementById('ballList').appendChild(listElement);
 }
-
-var coolifier = adjectifier('cool');
-
-console.log(coolifier('functions'));
-
-var rooms = ['h1', 'h2', 'h3'];
-var newRooms = rooms.map(function(rm){
-	if (rm === 'h3') { return 'h4'}
-	else { return rm}
-
-});
-
-console.log(rooms, newRooms);
