80664dd027f913ad3ab0efe0d0bfc122aa76a123
vikrantsingh13 <vikrantsinghvm@gmail.com>
2017-04-22 13:46:31 -0400

    more js

--- js/index.js
+++ js/index.js
@@ -5,3 +5,14 @@ for(var i = 0; i < typesOfBalls.length; i++){
 	listElement.innerHTML = typesOfBalls[i];
 	document.getElementById('ballList').appendChild(listElement);
 }
+
+function adjectifier(adjective){
+    return function(string){
+    	return adjective + " " + string;
+    }
+
+}
+
+var coolifier = adjectifier('cool');
+
+console.log(coolifier('javascript'));
