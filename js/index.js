ef5444f16519ba391d446cb25d6b0add3dba6dbf
vikrantsingh13 <vikrantsinghvm@gmail.com>
2017-05-11 21:22:29 -0400

    updated js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,4 @@
-var typesOfBalls = ['basket', 'base', 'foot', 'deez nuts'];
 
-for(var i = 0; i < typesOfBalls.length; i++){
-	var listElement = document.createElement('li');
-	listElement.innerHTML = typesOfBalls[i];
-	document.getElementById('ballList').appendChild(listElement);
-}
 
 function adjectifier(adjective){
     return function(string){
@@ -17,32 +11,12 @@ var coolifier = adjectifier('cool');
 
 console.log(coolifier('javascript'));
 
+window.onload = function(){
+  function callback(val){
+  	console.log(val);
+  }
+  var fruits = ['banana', 'apple', 'pear'];
+  fruits.forEach(callback);
+  console.log('done');
+};
 
-
-//wait for window to load
-window.addEventListener('load', registerEvents, false);
-
-//counter
-var currentCount = 0;
-
-/**
-Registers onclick events
-*/
-function registerEvents(e){
-	document.getElementById('incrementCounter').addEventListener('click', increaseCount, false);
-	document.getElementById('incrementCounter').addEventListener('click', increaseSize, false);
-}
-
-/**
-Increases currentCount and displays the result
-*/
-function increaseCount(e){
-	currentCount++;
-	document.getElementById('currentCount').innerHTML = currentCount;
-}
-/**
-Increases the font size of the count text
-*/
-function increaseSize(e){
-	document.getElementById("currentCount").style.fontSize = currentCount;
-}
