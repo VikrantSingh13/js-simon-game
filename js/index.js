7d8d40ca73374734523973df1bca352701b29a5f
vikrantsingh13 <vikrantsinghvm@gmail.com>
2017-07-06 20:37:27 -0400

    Working version of hard Simon Game

--- js/index.js
+++ js/index.js
@@ -1,22 +1,264 @@
+"use strict";
 
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { return step("next", value); }, function (err) { return step("throw", err); }); } } return step("next"); }); }; }
 
-function adjectifier(adjective){
-    return function(string){
-    	return adjective + " " + string;
-    }
+var greenQuad = document.querySelector("#q1");
+var redQuad = document.querySelector("#q2");
+var yellowQuad = document.querySelector("#q3");
+var blueQuad = document.querySelector("#q4");
+var gameQuads = [greenQuad, redQuad, yellowQuad, blueQuad];
+var quads = document.getElementsByClassName("quads");
 
-}
+var randomSequence = [];
+var currentScore = 0;
+var highScore = 0;
+var counter = 0;
+var isGameOver = false;
+var isDisplaying = false;
+//dom elements for scores
+var currentScoreEle = document.querySelector(".currentScore");
+var highScoreEle = document.querySelector(".highScore");
 
-var coolifier = adjectifier('cool');
+var gameAudio = new Audio('https://soundbible.com/grab.php?id=1598&type=mp3');
+var gameOverAudio = new Audio("https://soundbible.com/grab.php?id=1830&type=mp3");
+var highScoreAudio = new Audio("https://soundbible.com/grab.php?id=1964&type=mp3");
 
-console.log(coolifier('javascript'));
-
-window.onload = function(){
-  function callback(val){
-  	console.log(val);
-  }
-  var fruits = ['banana', 'apple', 'pear'];
-  fruits.forEach(callback);
-  console.log('done');
+var sleep = function sleep(time) {
+  return new Promise(function (resolve) {
+    return setTimeout(resolve, time);
+  });
 };
 
+window.onload = _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
+  var startButton;
+  return regeneratorRuntime.wrap(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.next = 2;
+          return sleep(1000);
+
+        case 2:
+          startButton = document.querySelector("#startButton");
+
+          startButton.addEventListener('click', function () {
+            currentScore = 0;
+            isGameOver = false;
+            startGame();
+          });
+
+        case 4:
+        case "end":
+          return _context.stop();
+      }
+    }
+  }, _callee, undefined);
+}));
+
+var startGame = function () {
+  var ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee2() {
+    var i;
+    return regeneratorRuntime.wrap(function _callee2$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            randomSequence = [];
+            _context2.next = 3;
+            return sleep(600);
+
+          case 3:
+            generateRandomSequence(currentScore + 1);
+            console.log(randomSequence);
+
+            updateCurrentScore();
+
+            isDisplaying = true;
+            i = 0;
+
+          case 8:
+            if (!(i < randomSequence.length)) {
+              _context2.next = 18;
+              break;
+            }
+
+            if (isGameOver) {
+              _context2.next = 15;
+              break;
+            }
+
+            _context2.next = 12;
+            return sleep(500);
+
+          case 12:
+            displayColor(randomSequence[i], 600);
+            _context2.next = 15;
+            return sleep(500);
+
+          case 15:
+            i++;
+            _context2.next = 8;
+            break;
+
+          case 18:
+            isDisplaying = false;
+
+          case 19:
+          case "end":
+            return _context2.stop();
+        }
+      }
+    }, _callee2, undefined);
+  })),
+      _this = undefined;
+
+  return function startGame() {
+    return ref.apply(_this, arguments);
+  };
+}();
+
+//displayColor takes in the quadrant to light up, and for how long
+var displayColor = function () {
+  var ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee3(quadrant, timer) {
+    return regeneratorRuntime.wrap(function _callee3$(_context3) {
+      while (1) {
+        switch (_context3.prev = _context3.next) {
+          case 0:
+            gameQuads[quadrant].classList.add("active");
+            gameAudio.currentTime = 0;
+            gameAudio.play();
+            _context3.next = 5;
+            return sleep(timer).then(function () {
+              gameQuads[quadrant].classList.remove("active");
+              gameAudio.pause();
+            });
+
+          case 5:
+          case "end":
+            return _context3.stop();
+        }
+      }
+    }, _callee3, undefined);
+  })),
+      _this = undefined;
+
+  return function displayColor(_x, _x2) {
+    return ref.apply(_this, arguments);
+  };
+}();
+
+var generateRandomSequence = function generateRandomSequence(n) {
+  for (var i = 0; i < n; i++) {
+    var m = Math.floor(Math.random() * 4 + 0);
+    randomSequence.push(m);
+  }
+};
+
+var checkUserInput = function () {
+  var ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee4(event) {
+    return regeneratorRuntime.wrap(function _callee4$(_context4) {
+      while (1) {
+        switch (_context4.prev = _context4.next) {
+          case 0:
+            if (isDisplaying) {
+              _context4.next = 18;
+              break;
+            }
+
+            console.log(event.target.id);
+            displayColor(parseInt(event.target.id.substring(1, 2)) - 1, 200);
+
+            _context4.next = 5;
+            return sleep(250);
+
+          case 5:
+            if (!("q" + (randomSequence[counter] + 1) !== event.target.id)) {
+              _context4.next = 12;
+              break;
+            }
+
+            console.log("it was q" + (randomSequence[counter] + 1));
+            counter = 0;
+            displayGameOver();
+            return _context4.abrupt("return");
+
+          case 12:
+            counter += 1;
+            //if the user has answered correctly we move to
+
+            if (!(counter > currentScore)) {
+              _context4.next = 18;
+              break;
+            }
+
+            currentScore = counter;
+            counter = 0;
+            startGame();
+            return _context4.abrupt("return");
+
+          case 18:
+          case "end":
+            return _context4.stop();
+        }
+      }
+    }, _callee4, undefined);
+  })),
+      _this = undefined;
+
+  return function checkUserInput(_x3) {
+    return ref.apply(_this, arguments);
+  };
+}();
+
+var displayGameOver = function () {
+  var ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee5() {
+    return regeneratorRuntime.wrap(function _callee5$(_context5) {
+      while (1) {
+        switch (_context5.prev = _context5.next) {
+          case 0:
+            isGameOver = true;
+            isDisplaying = false;
+
+            updateCurrentScore();
+
+            gameOverAudio.play();
+            alert("Game Over! You scored: " + currentScore + ". Press Start to play again.");
+            gameOverAudio.pause();
+            gameOverAudio.currentTime = 0;
+
+            _context5.next = 9;
+            return sleep(500);
+
+          case 9:
+            if (currentScore > highScore) {
+              highScore = currentScore;
+              updateHighScore();
+
+              highScoreAudio.play();
+              alert("New Highscore!!!");
+              highScoreAudio.pause();
+              highScoreAudio.currentTime = 0;
+            }
+
+          case 10:
+          case "end":
+            return _context5.stop();
+        }
+      }
+    }, _callee5, undefined);
+  })),
+      _this = undefined;
+
+  return function displayGameOver() {
+    return ref.apply(_this, arguments);
+  };
+}();
+
+var updateCurrentScore = function updateCurrentScore() {
+  currentScoreEle.innerHTML = currentScore;
+};
+
+var updateHighScore = function updateHighScore() {
+  highScoreEle.innerHTML = currentScore;
+};
+
+$('.quads').on('click', checkUserInput);
